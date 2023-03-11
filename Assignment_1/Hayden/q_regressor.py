9bfca0bf7658f64e8737bdb064ec3922b85c0579
Hayden2018 <39459225+Hayden2018@users.noreply.github.com>
2023-03-11 12:32:49 +0800

    Update q_regressor.py

--- Assignment_1/Hayden/q_regressor.py
+++ Assignment_1/Hayden/q_regressor.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 class QRegressor:
 
     def __init__(self, k=5, weights='distance'):
-        self.model = KNeighborsRegressor(k, weights)
+        self.model = KNeighborsRegressor(n_neighbors=k, weights=weights)
 
     # Wt, xt, r are 1D list
     def train(self, W, x, r):
@@ -14,18 +14,18 @@ class QRegressor:
         self.w_max = max(W)
         self.x_min = min(x)
         self.x_max = max(x)
-        self.w_std = np.std(W)
-        self.x_std = np.std(x)
-        self.W = np.array(W) / self.w_std
-        self.x = np.array(x) / self.x_std
+        self.W = W
+        self.x = x
         self.segment = (self.x_max - self.x_min) / 200
         self.model.fit(np.array([W, x]).transpose(), r)
 
-
+    # Wt and xt can be scalar or list
     def estimate(self, Wt, xt):
-        Wt = np.array(Wt) / self.w_std
-        xt = np.array(xt) / self.x_std
-        return self.model.predict(np.array([Wt, xt]).transpose())
+        pred = self.model.predict(np.array([Wt, xt]).reshape(2, -1).transpose())
+        if len(pred) == 1:
+            return pred[0]
+        else:
+            return pred
         
     # Input Wt to get x that maximize reward and the corresponding reward
     def find_max(self, Wt):
@@ -40,28 +40,27 @@ class QRegressor:
         return x_max, r_max
         
     def plot(self):
-        if self.interp is None:
-            raise Exception('Interpolator not yet trained')
-        else:
-            W = np.linspace(self.w_min, self.w_max)
-            X = np.linspace(self.x_min, self.x_max)
-            W, X = np.meshgrid(W, X)
-            R = self.interp(W, X)
-            plt.pcolormesh(W, X, R, shading='auto')
-            plt.plot(self.Wt, self.xt, "ok", label='input point')
-            plt.legend()
-            plt.colorbar()
-            plt.axis("equal")
-            plt.show()
+        W = np.linspace(self.w_min, self.w_max)
+        X = np.linspace(self.x_min, self.x_max)
+        W, X = np.meshgrid(W, X)
+        # R = 1- np.hypot(W, X)
+        R = self.estimate(W.reshape(-1), X.reshape(-1))
+        R = R.reshape(50, 50)
+        plt.pcolormesh(W, X, R, shading='auto')
+        plt.plot(self.W, self.x, "ok", label='input point')
+        plt.legend()
+        plt.colorbar()
+        plt.axis("equal")
+        plt.show()
 
 
 if __name__ == '__main__':
     rng = np.random.default_rng()
-    w = rng.random(20) - 0.5
-    x = rng.random(20) - 0.5
+    w = rng.random(500) - 0.5
+    x = rng.random(500) - 0.5
     r = 1 - np.hypot(w, x)
 
-    interpo = QRegressor()
-    interpo.train(w, x, r)
-    interpo.plot()
-    # print(interpo.find_max(0))
+    model = QRegressor()
+    model.train(w, x, r)
+    model.plot()
+    print(model.find_max(0))
