82b449d35f2a384d0dea155334ba3c8fc2f644c3
Hayden2018 <39459225+Hayden2018@users.noreply.github.com>
2023-05-04 18:59:50 +0800

    Add in-progress code

--- Assignment_2/hayden_test.py
+++ Assignment_2/hayden_test.py
@@ -2,14 +2,16 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
+from random import random, seed
+
 
 # Pytorch model boiler plate
 class Policy(nn.Module):
 
     def __init__(self):
         super(Policy, self).__init__()
-        self.fc1 = nn.Linear(2, 16)
-        self.fc2 = nn.Linear(16 ,1)
+        self.fc1 = nn.Linear(2, 32)
+        self.fc2 = nn.Linear(32 ,1)
         
     def forward(self, up_price, down_price, exercise_price):
         up_ratio = up_price / exercise_price
@@ -17,24 +19,22 @@ class Policy(nn.Module):
 
         x = torch.stack([up_ratio, down_ratio], dim=1)
         return self.fc2(torch.tanh(self.fc1(x)))
-    
 
-from random import random
+def analytical_answer(up_price, down_price, exercise_price):
+    payout_up = torch.where(exercise_price > up_price, exercise_price - up_price, torch.zeros_like(exercise_price))
+    payout_down = torch.where(exercise_price > down_price, exercise_price - down_price, torch.zeros_like(exercise_price))
+    return (payout_up - payout_down) / (up_price - down_price)
 
-up_factor = 1.2
+up_factor = 1.1
 down_factor = 1 / up_factor
-risk_free_rate = 0.05
-exercise_price = 0.6
+risk_free_rate = 0.01
+exercise_price = 0.95
 
 risk_neutral_prob = (1 + risk_free_rate - down_factor) / (up_factor - down_factor)
 
-
 def generate_simulation():
 
-    up_price = up_factor
-    down_price = down_factor
     current_price = 1
-
     up_prices = []
     down_prices = []
     previous_prices = []
@@ -42,8 +42,8 @@ def generate_simulation():
     
     for _ in range(9):
         previous_prices.append(current_price)
-        up_prices.append(up_price)
-        down_prices.append(down_price)
+        up_prices.append(current_price * up_factor)
+        down_prices.append(current_price * down_factor)
         if random() < risk_neutral_prob:
             current_price *= up_factor
         else:
@@ -65,7 +65,6 @@ for _ in range(20):
     previous_prices.extend(pp)
     next_prices.extend(np)
 
-
 up_prices = torch.tensor(up_prices)
 down_prices = torch.tensor(down_prices)
 exercise_price = torch.tensor(exercise_price)
@@ -73,14 +72,24 @@ previous_prices = torch.tensor(previous_prices)
 next_prices = torch.tensor(next_prices)
 
 policy = Policy()
-optimizer = optim.SGD(policy.parameters(), lr=0.001)
+optimizer = optim.SGD(policy.parameters(), lr=0.0002)
 
 def loss_function(hedge_ratio, previous_price, next_price, exercise_price):
-    payoff = torch.where(exercise_price > next_price, exercise_price - next_price, torch.zeros_like(exercise_price))
-    position_change = payoff + hedge_ratio * (next_price - previous_price)
-    return torch.abs(position_change)
+    payout = torch.where(exercise_price > next_price, exercise_price - next_price, torch.zeros_like(next_price))
+    position_change = hedge_ratio * (next_price - previous_price) - payout
+    return torch.abs(position_change).mean()
 
-num_epochs = 100
+# ans = analytical_answer(torch.tensor(1.3), torch.tensor(1 / 1.3), torch.tensor(0.9))
+# uloss = loss_function(ans, torch.tensor(1), torch.tensor(1.3), torch.tensor(0.9))
+# dloss = loss_function(ans, torch.tensor(1), torch.tensor(1 / 1.3), torch.tensor(0.9))
+# print(ans, uloss, dloss)
+
+num_epochs = 2000
+answer = analytical_answer(up_prices, down_prices, exercise_price)
+print(answer)
+min_loss = loss_function(answer, previous_prices, next_prices, exercise_price)
+print(min_loss)
+
 for epoch in range(num_epochs):
     # Zero the gradients
     optimizer.zero_grad()
@@ -97,4 +106,10 @@ for epoch in range(num_epochs):
     # Update the weights
     optimizer.step()
 
+    if (epoch + 1) % 5 == 0:
+        print(f'Epoch {epoch + 1} loss:', float(loss))
+        # print(torch.linalg.vector_norm(outputs - answer))
 
+
+outputs = policy(up_prices, down_prices, exercise_price)
+print(outputs.tolist())
